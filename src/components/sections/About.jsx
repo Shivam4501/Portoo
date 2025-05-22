a3988fbfad038146aa6b63630736b7ab98f6d2cd
Shivam Sharma <jtrshivamsharma@gmail.com>
2025-05-22 19:57:44 +0530

    Fix casing of OtherSkills and add display section in About component

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -8,7 +8,7 @@ export const About = () => {
   ];
 
   const backendSkills = ["Node.js", "AWS", "MongoDB", "Express.js", "Mongoose"];
-  const otherSkills = [
+  const OtherSkills = [
     "C++",
     "Data Structures",
     "Algorithms",
@@ -65,9 +65,28 @@ export const About = () => {
                   ))}
                 </div>
               </div>
+
+                   <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> OtherSkills</h3>
+                <div className="flex flex-wrap gap-2">
+                  {OtherSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
             </div>
           </div>
 
+         
+            
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
             <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
               <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
